611350a5a3e0a1917074a4f7ce88fa751d5419f5
Ludo <lud0@users.noreply.github.com>
2018-12-17 23:11:09 +0100

    Added STATIC_ROOT for staticfiles app and added more files to gitignore

--- mkrk/settings.py
+++ mkrk/settings.py
@@ -176,7 +176,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Celery settings
